a63dc0348f2fdef85740f7ed410a19c1e786b151
taki pc <taki pc@DESKTOP-EBSRAG6>
2026-08-05 16:15:59 +0100

    fg

--- DevExpress.ProductsDemo.Win/Program.cs
+++ DevExpress.ProductsDemo.Win/Program.cs
@@ -56,13 +56,13 @@ namespace DevExpress.ProductsDemo.Win {
             // the login dialog's parent for centering), but keep it disabled/
             // hidden from actual use until login succeeds.
             mainForm.Show();
-            mainForm.Enabled = false;
+            //mainForm.Enabled = false;
 
             bool loggedIn;
             using (var loginForm = new DevExpress.ProductsDemo.Win.Forms.frmLogin())
             {
-                loggedIn = loginForm.ShowDialog(mainForm) == DialogResult.OK;
-               // loggedIn = true;
+               // loggedIn = loginForm.ShowDialog(mainForm) == DialogResult.OK;
+                loggedIn = true;
             }
 
             if (!loggedIn)
